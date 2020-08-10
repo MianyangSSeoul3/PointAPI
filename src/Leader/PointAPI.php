9e458b0a4640905b73b364b99ac0555d419df65a
Jjun18 <52364897+MianyangSSeoul3@users.noreply.github.com>
2020-08-10 17:42:09 +0900

    Source code cleanup

--- src/Leader/PointAPI.php
+++ src/Leader/PointAPI.php
@@ -9,60 +9,70 @@ use pocketmine\utils\Config;
 use pocketmine\Player;
 use pocketmine\Server;
 
-class PointAPI extends PluginBase implements Listener {
-  
-  public $Pt;
-  
-  private static $instance;
+class PointAPI extends PluginBase implements Listener
+{
 
-  public static function getInstance() : self {
+    public $Pt;
+
+    private static $instance;
+
+    public static function getInstance(): self
+    {
         return self::$instance;
     }
 
-  public function onLoad(){
-		self::$instance = $this;
+    public function onLoad()
+    {
+        self::$instance = $this;
 
-	}
-  
-  public function onEnable() {
-  $this->getServer()->getPluginManager()->registerEvents($this, $this);
-	$this->getServer()->getLogger()->alert("[ LD_Point ] 포인트 플러그인");
-	@mkdir($this->getDataFolder());
-	$this->Point = new Config($this->getDataFolder()."PointAPI.yml",Config::YAML);
-	$this->Pt = $this->Point->getAll();
-  }
-  
-  public function onDisable() {
-    $this->Point->setAll($this->Pt);
-		$this->Point->save();
-  }
-  
-  public function Join(PlayerJoinEvent $event) {
-    $player = $event->getPlayer();
-    $name = $player->getName();
-    $player->sendMessage("§l§b[ Point ]§r§7 포인트플러그인을 사용중입니다.");
-		if(!isset($this->Pt[$name])) { 
-    $this->Pt[strtolower($name)]["포인트"] = "0";
-    } 
-  }
-  
-  public function getPoint(Player $player) {
-		$name = $player->getName();
-		return $this->Pt[strtolower($name)]["포인트"];
-	}
+    }
 
-  public function addPoint(Player $player , int $Point) {
-    $name = $player->getName();
-    $this->Pt[strtolower($name)]["포인트"] += "{$Point}";
-  }
-  
-  public function setPoint(Player $player , int $Point) {
-    $name = $player->getName();
-    $this->Pt[strtolower($name)]["포인트"] = "{$Point}";
-  }
-  
-  public function stPoint(Player $player , int $Point ) {
-    $name = $player->getName();
-    $this->Pt[strtolower($name)]["포인트"] -= "{$Point}";
-  }
+    public function onEnable()
+    {
+        $this->getServer()->getPluginManager()->registerEvents($this, $this);
+        $this->getServer()->getLogger()->alert("[ LD_Point ] 포인트 플러그인");
+        @mkdir($this->getDataFolder());
+        $this->Point = new Config($this->getDataFolder() . "PointAPI.yml", Config::YAML);
+        $this->Pt = $this->Point->getAll();
+    }
+
+    public function onDisable()
+    {
+        $this->Point->setAll($this->Pt);
+        $this->Point->save();
+    }
+
+    public function Join(PlayerJoinEvent $event)
+    {
+        $player = $event->getPlayer();
+        $name = $player->getName();
+        $player->sendMessage("§l§b[ Point ]§r§7 포인트플러그인을 사용중입니다.");
+        if (!isset($this->Pt[$name])) {
+            $this->Pt[strtolower($name)]["포인트"] = "0";
+        }
+    }
+
+    public function getPoint(Player $player)
+    {
+        $name = $player->getName();
+        return $this->Pt[strtolower($name)]["포인트"];
+    }
+
+    public function addPoint(Player $player, int $Point)
+    {
+        $name = $player->getName();
+        $this->Pt[strtolower($name)]["포인트"] += "{$Point}";
+    }
+
+    public function setPoint(Player $player, int $Point)
+    {
+        $name = $player->getName();
+        $this->Pt[strtolower($name)]["포인트"] = "{$Point}";
+    }
+
+    public function stPoint(Player $player, int $Point)
+    {
+        $name = $player->getName();
+        $this->Pt[strtolower($name)]["포인트"] -= "{$Point}";
+    }
 }
